a55a2acd2da1589f035924dccc4dee1a77421135
drewtchrist <andrewtaylorchristiansen@gmail.com>
2020-12-24 14:11:24 -0700

    Sensor routes now update list of processors in processor manager.

--- server/avista_iot/server.py
+++ server/avista_iot/server.py
@@ -8,24 +8,24 @@ class IoTServer(Service):
     def __init__(self):
         """ instantiate the app """
         super().__init__()
-        self._processor_manager = None
+        self.processor_manager = None
 
     def _setup_endpoints(self):
         super()._setup_endpoints()
         self._app.register_blueprint(api.api_bp)
-        self._processor_manager = ProcessorManager(self._app, self._config)
+        self.processor_manager = ProcessorManager(self._app, self._config)
+        self.processor_manager.init()
 
     def start(self):
         """ starts the server """
         super().start()
-        self._processor_manager.init()
-        self._processor_manager.run()
+        self.processor_manager.run()
         # this is where to add access to the ProcessorManager()
 
     def stop(self):
         """ Stops the server """
         super().stop()
-        self._processor_manager.stop()
+        self.processor_manager.stop()
         # Also need to kill the ProcessorManager() here
 
     def check_status(self):
