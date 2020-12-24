3ecedee628ea387af919e6ba7b636d6dc845eea8
drewtchrist <andrewtaylorchristiansen@gmail.com>
2020-12-24 15:57:53 -0700

    Profile view has been updated to use cards to organize information.

--- client/src/views/Profile.vue
+++ client/src/views/Profile.vue
@@ -1,23 +1,35 @@
 <template>
-  <b-container class="text-left">
+  <b-container>
     <b-jumbotron class="text-center">
       <template #header>
         {{ currentUser.first_name }} {{ currentUser.last_name }}
       </template>
+
+      <div class="text-left">
+        <b-card title="Profile Information" class="mb-2">
+          <b-card-text>
+            <strong>Email Address: </strong>{{ currentUser.email }}
+          </b-card-text>
+          <b-card-text>
+            <strong>Roles: </strong>
+            <ul>
+              <li>{{ currentUser.role }}</li>
+            </ul>
+          </b-card-text>
+        </b-card>
+
+        <b-card title="Token">
+          <b-button v-b-toggle.collapse-1 variant="success">Show Token</b-button>
+          <b-collapse id="collapse-1" class="mt-2">
+            <b-card-text>
+              {{ currentUser.token.substring(0, 20) }} ...
+              {{ currentUser.token.substr(currentUser.token.length - 20) }}
+            </b-card-text>
+          </b-collapse>
+        </b-card>
+      </div>
+
     </b-jumbotron>
-    <p>
-      <strong>Token:</strong>
-      {{ currentUser.token.substring(0, 20) }} ...
-      {{ currentUser.token.substr(currentUser.token.length - 20) }}
-    </p>
-    <p>
-      <strong>Email:</strong>
-      {{ currentUser.email }}
-    </p>
-    <strong>Roles:</strong>
-    <ul>
-      <li v-for="(role,index) in currentUser.roles" :key="index">{{role}}</li>
-    </ul>
   </b-container>
 </template>
 
