63d6b3d6d1bdb7a7aa8a5c493ab1ffc2b83cfbde
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2021-02-07 19:15:54 -0700

    ready for release 0.2.4

--- server/setup.py
+++ server/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="avista_iot",
-    version="0.2.3",
+    version="0.2.4",
     author="Isaac Griffith",
     author_email="grifisaa@isu.edu",
     description="",
